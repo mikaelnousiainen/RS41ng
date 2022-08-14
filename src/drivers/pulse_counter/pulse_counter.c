ef699ef3a3109fd533f3c7c12c8d4d55a9c39e56
Manoel <manoel.casquilho@gmail.com>
2022-08-14 17:26:33 +0200

    Interrupt implementation

--- src/drivers/pulse_counter/pulse_counter.c
+++ src/drivers/pulse_counter/pulse_counter.c
@@ -1,43 +1,63 @@
 #include "pulse_counter.h"
-#include "stm32f10x_tim.h"
+#include "stm32f10x_exti.h"
 #include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
 #include "stm32f10x.h"
 #include "misc.h"
 
+uint16_t counts=1666;
 
 bool pulse_counter_init()
 {
 	//TODO: start counter.
-	GPIOBinit_TIM2(void);
-	TIM2init_counter(void);
-
+	GPIO_Init();
     return true;
 }
 
 uint16_t pulse_counter_get_counts(){
 	//TODO: Retrieve total counts and return it.
-	return 1666;
+	return counts;
 }
 
-void GPIOBinit_TIM2(void) {
-	GPIO_InitTypeDef GPIO_InitStructure; // 2
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE); // 4
-	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_TIM2); // PB11:TIM2_Ch4/ETR // 6
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11; // 8
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF; // 9
-	GPIO_Init(GPIOA, &GPIO_InitStructure); // 10
+void GPIO_Init(void) {
+	
+	GPIO_InitTypeDef GPIO_InitStructure; 
+	//Set pin 22 as input
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_22;
+	//Enable internal pullup
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	
+	
+	
+	EXTI_InitTypeDef EXTI_InitStruct;
+	/* PD0 is connected to EXTI_Line0 */
+	EXTI_InitStruct.EXTI_Line = EXTI_Line11;
+	/* Interrupt mode */
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	/* Triggers on rising and falling edge */
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+	/* Enable interrupt */
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	/* Add to EXTI */
+	EXTI_Init(&EXTI_InitStruct);
+	
+	
+	
+	/* Add IRQ vector to NVIC */
+	/* PB11 is connected to EXTI_Line11, which has EXTI11_IRQn vector */
+	NVIC_InitStruct.NVIC_IRQChannel = EXTI11_IRQn;
+	/* Set priority */
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x00;
+	/* Set sub priority */
+	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x00;
+	/* Enable interrupt */
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	/* Add to NVIC */
+	NVIC_Init(&NVIC_InitStruct);
 }
 
-
-void TIM2init_counter(void) { // counting from ETR
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure; // 2
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // 4
-	TIM_TimeBaseInitStructure.TIM_Prescaler = 0; // 6
-	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; // 7
-	TIM_TimeBaseInitStructure.TIM_Period = 100000; // 8
-	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 9
-	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure); // 10
-	TIM_ETRClockMode2Config(TIM2, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_NonInverted, 0x00); // 12
-	TIM_Cmd(TIM2, ENABLE); // 14
+void EXTI11_IRQHandler(void)
+{
+	counts = counts+1;
+	EXTI_ClearITPendingBit(EXTI_Line11); //Clear the interrupt flag bit on LINE11
 }
--- src/drivers/pulse_counter/pulse_counter.h
+++ src/drivers/pulse_counter/pulse_counter.h
@@ -3,5 +3,4 @@
 
 bool pulse_counter_init();
 uint16_t pulse_counter_get_counts();
-void GPIOBinit_TIM2(void);
-void TIM2init_counter(void);
+void GPIO_Init();
