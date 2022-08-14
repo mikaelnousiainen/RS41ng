ac4a79feeb4d1830c1978bea53816a109eaea54f
Manoel <manoel.casquilho@gmail.com>
2022-08-14 15:28:49 +0200

    Counter tests

--- src/drivers/pulse_counter/pulse_counter.c
+++ src/drivers/pulse_counter/pulse_counter.c
@@ -1,16 +1,44 @@
 #include "pulse_counter.h"
-#include "stm32f10x_exti.h"
+#include "stm32f10x_tim.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_rcc.h"
 #include "stm32f10x.h"
 #include "misc.h"
 
 
 bool pulse_counter_init()
 {
-	//TODO: start interrupt handling, attach it to the designated pin.
+	//TODO: start counter.
+	GPIOBinit_TIM2(void);
+	TIM2init_counter(void);
+
     return true;
 }
 
 uint16_t pulse_counter_get_counts(){
 	//TODO: Retrieve total counts and return it.
 	return 1666;
+}
+
+void GPIOBinit_TIM2(void) {
+	GPIO_InitTypeDef GPIO_InitStructure; // 2
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE); // 4
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_TIM2); // PB11:TIM2_Ch4/ETR // 6
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11; // 8
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF; // 9
+	GPIO_Init(GPIOA, &GPIO_InitStructure); // 10
+}
+
+
+void TIM2init_counter(void) { // counting from ETR
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure; // 2
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // 4
+	TIM_TimeBaseInitStructure.TIM_Prescaler = 0; // 6
+	TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up; // 7
+	TIM_TimeBaseInitStructure.TIM_Period = 100000; // 8
+	TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1; // 9
+	Timer – counting pulses 4
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStructure); // 10
+	TIM_ETRClockMode2Config(TIM2, TIM_ExtTRGPSC_OFF, TIM_ExtTRGPolarity_NonInverted, 0x00); // 12
+	TIM_Cmd(TIM2, ENABLE); // 14
 }
--- src/drivers/pulse_counter/pulse_counter.h
+++ src/drivers/pulse_counter/pulse_counter.h
@@ -3,3 +3,5 @@
 
 bool pulse_counter_init();
 uint16_t pulse_counter_get_counts();
+void GPIOAinit_TIM2(void);
+void TIM2init_counter(void);
