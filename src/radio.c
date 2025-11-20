3a67d8d90c1fab41b85c6954542350f1cb51cd11
Andrew Koenig <ke5gdb@gmail.com>
2025-11-19 22:58:58 -0600

    disable GPS stuff for now

--- src/radio.c
+++ src/radio.c
@@ -693,10 +693,10 @@ static bool radio_start_transmit(radio_transmit_entry *entry)
             return false;
     }
 
-    usart_gps_enable(enable_gps_during_transmit);
-    if (!enable_gps_during_transmit) {
-        ubxg6010_reset_parser();
-    }
+    // usart_gps_enable(enable_gps_during_transmit);
+    // if (!enable_gps_during_transmit) {
+    //     ubxg6010_reset_parser();
+    // }
 
     switch (entry->radio_type) {
 #ifdef RS41
@@ -717,7 +717,7 @@ static bool radio_start_transmit(radio_transmit_entry *entry)
     }
 
     if (!success) {
-        usart_gps_enable(true);
+        // usart_gps_enable(true);
         // TODO: stop transmit here
         return false;
     }
@@ -809,7 +809,7 @@ static bool radio_stop_transmit(radio_transmit_entry *entry)
             return false;
     }
 
-    usart_gps_enable(true);
+    // usart_gps_enable(true);
     if (leds_enabled) {
         set_red_led(false);
     }
