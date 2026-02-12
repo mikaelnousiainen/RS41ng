dd5245cd8db378672de05fcfd98a9fc7c73ad9a9
Andrew Koenig <ke5gdb@gmail.com>
2026-02-11 23:06:11 -0600

    Update DFM config, return voltage back to full scale

--- src/hal/system.c
+++ src/hal/system.c
@@ -212,12 +212,11 @@ static void dma_adc_init()
 uint16_t system_get_battery_voltage_millivolts()
 {
 #ifdef RS41
-    return (uint16_t) (((float) dma_buffer_adc[0]) * 10.0f * 600.0f / 4096.0f);
+    return (uint16_t)((uint32_t)dma_buffer_adc[0] * 6000UL / 4096UL);
 #else  //DFM17
-    // Kludge:  DFM17 voltage is more than 5 volts, so cut the value in half.
     // Changed 600 to 690 to try and get it more accurate to actual voltage
     // 690 yields fairly linear, accurate results between 5v6 and 6v6
-    return (uint16_t) ( ((float) dma_buffer_adc[0]) * 10.0f * 690.0f / 8192.0f );
+    return (uint16_t)((uint32_t)dma_buffer_adc[0] * 6900UL / 4096UL);
 #endif 
 }
 
